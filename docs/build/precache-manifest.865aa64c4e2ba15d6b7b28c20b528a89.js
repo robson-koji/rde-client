415cc2d103f05cdf21c6c800397b734d1bf99a7c
robson.koji <rbsnkjmr@gmail.com>
2022-05-03 20:27:17 -0300

    gh-pages

---
+++ docs/build/precache-manifest.865aa64c4e2ba15d6b7b28c20b528a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d567ca7d00778a9783d4",
+    "url": "/static/js/runtime~main.d567ca7d.js"
+  },
+  {
+    "revision": "743aafe297bffe552ebb",
+    "url": "/static/js/main.743aafe2.chunk.js"
+  },
+  {
+    "revision": "0e725d8ef6c7ac39d055",
+    "url": "/static/js/2.0e725d8e.chunk.js"
+  },
+  {
+    "revision": "1d6c1b37193d9484c3b3",
+    "url": "/static/js/1.1d6c1b37.chunk.js"
+  },
+  {
+    "revision": "743aafe297bffe552ebb",
+    "url": "/static/css/main.3aaeea6f.chunk.css"
+  },
+  {
+    "revision": "aeeab2ba400b60c6730577adbf919c4d",
+    "url": "/index.html"
+  }
+];
